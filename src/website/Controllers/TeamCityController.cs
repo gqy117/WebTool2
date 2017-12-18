2e9e449c45e4a239cfad9fdaab81de4e7b5c9c4c
Leo Li <gqy117@gmail.com>
2017-12-18 11:03:34 +0000

    Animation

--- src/website/Controllers/TeamCityController.cs
+++ src/website/Controllers/TeamCityController.cs
@@ -11,16 +11,33 @@ namespace AdvancedPeopleDashboard.Controllers
     {
         private IList<Build> Builds = new List<Build>
         {
-            new Build { BuildId = "DCEMAIN", BuildStatus = "Successful", BuildName = "DCE Main", LastUpdated = DateTime.Now },
-            new Build { BuildId = "DCEUNITTESTS", BuildStatus = "Successful", BuildName = "DCE Unit Tests", LastUpdated = DateTime.Now },
-            new Build { BuildId = "DCEPOSTMAN", BuildStatus = "Successful", BuildName = "DCE Postman", LastUpdated = DateTime.Now },
-            new Build { BuildId = "ADVANCEDMAIN", BuildStatus = "Successful", BuildName = "Advanced Main", LastUpdated = DateTime.Now },
+            new Build { BuildId = "DCEMAIN", BuildName = "DCE Main", LastUpdated = DateTime.Now },
+            new Build { BuildId = "DCEUNITTESTS", BuildName = "DCE Unit Tests", LastUpdated = DateTime.Now },
+            new Build { BuildId = "DCEPOSTMAN", BuildName = "DCE Postman", LastUpdated = DateTime.Now },
+            new Build { BuildId = "ADVANCEDMAIN", BuildName = "Advanced Main", LastUpdated = DateTime.Now },
         };
+
+        private static int randomIndex = 0;
+        private static IList<string> StatusList = new List<string>{ "Successful", "Failed" };
 
         [HttpGet("[action]")]
         public Build Build(string buildId)
         {
-            return Builds.SingleOrDefault(x => x.BuildId == buildId);
+            var build = SetRandomStatus();
+
+            return build.SingleOrDefault(x => x.BuildId == buildId);
+        }
+
+        private IEnumerable<Build> SetRandomStatus()
+        {
+            randomIndex = randomIndex == 0 ? 1 : 0;
+
+            var build = Builds.Select(x =>
+            {
+                x.BuildStatus = StatusList[randomIndex];
+                return x;
+            });
+            return build;
         }
     }
 }
