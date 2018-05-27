f129ed1e565d1ae01997ede4c3e89b3236721a69
Leo Li <gqy117@gmail.com>
2018-05-27 19:09:10 +1200

    [ElasticSearch] Try deploy section.

--- scripts/5.docker.ps1
+++ scripts/5.docker.ps1
@@ -1,5 +1,3 @@
-if [ $TRAVIS_BRANCH == 'master' ]; then
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker build -t gqy117/webtool2 ../
-  docker push gqy117/webtool2
-fi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t gqy117/webtool2 ../
+docker push gqy117/webtool2
