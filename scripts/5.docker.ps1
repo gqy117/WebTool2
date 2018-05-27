f37f4312e2ec8a7b7cefaf93f1b353509e5a214e
Leo Li <gqy117@gmail.com>
2018-05-27 18:59:12 +1200

    [ElasticSearch] Write check if it's master branch in ps script.

--- scripts/5.docker.ps1
+++ scripts/5.docker.ps1
@@ -1,3 +1,5 @@
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t gqy117/webtool2 ../
-docker push gqy117/webtool2
+if [ $TRAVIS_BRANCH == 'master' ]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker build -t gqy117/webtool2 ../
+  docker push gqy117/webtool2
+fi
