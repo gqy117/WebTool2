c3008defa63c9224c7fbc79762466960034f2d10
Leo Li <gqy117@gmail.com>
2018-06-24 14:51:48 +1200

    [Get] Pushed state to url.

--- src/website/ClientApp/actions/Contacts.ts
+++ src/website/ClientApp/actions/Contacts.ts
@@ -1,10 +1,15 @@
-﻿import * as Model from "../models/Contacts";
+﻿import * as queryString from "query-string";
+import { push } from "react-router-redux";
+import * as Model from "../models/Contacts";
 import { ApplicationState } from "../reducers";
 
 export const actionCreators = {
     queryContacts: (name: string, value: string) => (dispatch: any, getState: () => ApplicationState) => {
         const queryChangeAction: Model.QueryChangedAction = { type: Model.ActionType.QUERY_CHANGED, name, value };
         dispatch(queryChangeAction);
+
+        const newRoute = queryString.stringify(getState().contacts.query);
+        dispatch(push(`/?${newRoute}`));
 
         const requestContactsAction: Model.RequestContactsAction = { type: Model.ActionType.REQUEST_CONTACT, contactQuery: getState().contacts.query as Model.ContactQuery };
         dispatch(requestContactsAction);
