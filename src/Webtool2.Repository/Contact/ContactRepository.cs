6b8d727eaaa8928e0d7c375cf790061faf2a32c6
Leo Li <gqy117@gmail.com>
2018-06-24 20:31:41 +1200

    [Get] Made a static method.

--- src/Webtool2.Repository/Contact/ContactRepository.cs
+++ src/Webtool2.Repository/Contact/ContactRepository.cs
@@ -4,6 +4,13 @@
 
     public abstract class ContactRepository
     {
+        public static int CalculatePage(int pages, int page)
+        {
+            int newPage = page > pages - 1 ? pages - 1 : page;
+
+            return Math.Max(0, newPage);
+        }
+
         protected int CalculatePages(int count, int pageSize)
         {
             return this.CalculatePages((long)count, pageSize);
@@ -12,12 +19,6 @@
         protected int CalculatePages(long count, int pageSize)
         {
             return (int)Math.Ceiling((decimal)count / (decimal)pageSize);
-        }
-
-        protected int CalculatePage(int pages, int page)
-        {
-            page = page > pages - 1 ? pages - 1 : page;
-            return Math.Max(0, page);
         }
     }
 }
